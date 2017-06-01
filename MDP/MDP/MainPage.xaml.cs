036ec871eeffa5d32559d2dcdd252242988d2e89
Osnofaz <Osnofaz@OSNOFAZLAPTOP>
2017-06-01 19:34:23 +0100

    Add project files.

---
+++ MDP/MDP/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MDP
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Master = new Master();
+            Detail = new NavigationPage(new Detail()) { 
+                BarBackgroundColor = Color.FromHex("#006660"),
+                BarTextColor = Color.White
+
+            };
+
+            App.mdp = this;
+        }
+    }
+}
---
+++ MDP/MDP/Master.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MDP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPage1_Clicked(object sender, EventArgs e)
+        {
+            App.mdp.IsPresented = false;
+            App.mdp.Detail.Navigation.PushAsync(new Page1());
+        }
+
+        private void btnPage2_Clicked(object sender, EventArgs e)
+        {
+            App.mdp.IsPresented = false;
+            App.mdp.Detail.Navigation.PushAsync(new Page2());
+        }
+    }
+}
