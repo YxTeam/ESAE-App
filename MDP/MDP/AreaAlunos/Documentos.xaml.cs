df159bd6687d3479fb734147979c38d93c9a971f
Filipe Silva <infinitywarfare7@gmail.com>
2017-07-18 15:47:04 +0100

    Minor text fixes

--- MDP/MDP/AreaAlunos/Documentos.xaml.cs
+++ MDP/MDP/AreaAlunos/Documentos.xaml.cs
@@ -1,20 +1,33 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MDP.Controllers;
 
 namespace MDP.AreaAlunos
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Documentos : ContentPage
 	{
-		public Documentos ()
+        DocumentoController documentos;
+
+        public Documentos ()
 		{
 			InitializeComponent ();
-		}
+
+            documentos = new DocumentoController();
+            exemplo.ItemsSource = documentos.ListaDocumentos; //adiciona os objetos à listview
+
+            exemplo.ItemSelected += (sender, e) => {
+                ((ListView)sender).SelectedItem = null;
+            };
+        }
 	}
 }
